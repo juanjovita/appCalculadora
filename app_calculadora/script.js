115bafa54488ddc9224d67c7c75df3498599161f
Juan Jovita <92692485+juanjovita@users.noreply.github.com>
2023-01-08 22:18:45 -0300

    Create script.js

---
+++ app_calculadora/script.js
@@ -0,0 +1,13 @@
+function calcular(tipo,valor){
+
+    if(tipo === 'acao'){
+      document.getElementById('resultado').value += valor
+    } else if(tipo ==='valor'){
+      document.getElementById('resultado').value += valor
+    }
+  }
+
+  function limpar() {
+    document.getElementById('resultado').value = ''
+    
+  }
